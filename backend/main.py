9a64c820e3c7dc05832d8329e53e675ce5b283fa
h97704539-code <h97704539@gmail.com>
2026-01-04 10:23:46 -0800

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI(title="YT Downloader Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
